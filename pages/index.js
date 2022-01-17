9907284ec079731ca11bcd140aec2fed739e3d88
Sash Patel <bhoolku5443@gmail.com>
2022-01-17 02:31:30 -0500

    google analytics

--- pages/index.js
+++ pages/index.js
@@ -47,9 +47,7 @@ export default function MainGame({ puzzle, emoji }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-              });
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
             `,
         }}
         />
