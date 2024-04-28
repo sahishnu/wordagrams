d63b58855c45fe0766e1cba3b7254ce7053dce3e
Sahishnu <9358407+sahishnu@users.noreply.github.com>
2024-04-28 11:56:14 -0400

    Remove By Sahishnu

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -31,8 +31,6 @@ export const Header = () => {
       />
       <h1 className={styles.title}>
         {META_CONTENT.title}
-        <br />
-        <span className={styles.byline}>By Sahishnu</span>
       </h1>
       {/*  */}
       <img
